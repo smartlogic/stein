f4f2f6d3e26bb67b49c93bd9195605fb02a0072b
Eric Oestrich <eric@oestrich.org>
2019-03-06 16:46:43 -0500

    Add typespecs and docs

--- lib/stein.ex
+++ lib/stein.ex
@@ -2,4 +2,11 @@ defmodule Stein do
   @moduledoc """
   Documentation for Stein.
   """
+
+  @typedoc """
+  Your Ecto repo module
+  """
+  @type repo() :: Ecto.Repo.t()
+
+  @type uuid() :: String.t()
 end
--- lib/stein/accounts.ex
+++ lib/stein/accounts.ex
@@ -1,19 +1,49 @@
 defmodule Stein.Accounts do
   @moduledoc """
   Helper functions around user accounts
+
+  To fully utilize the `Stein.Accounts` functions, your user schema struct
+  should contain the following fields:
+
+  ```elixir
+  schema "users" do
+    field(:email, :string)
+    field(:password, :string, virtual: true)
+    field(:password_hash, :string)
+    field(:email_verification_token, :string)
+    field(:email_verified_at, :utc_datetime)
+  end
+  ```
   """
 
-  @type repo() :: Ecto.Repo.t()
-  @type schema() :: Ecto.Schema.t()
-  @type changeset() :: Ecto.Changeset.t()
-  @type uuid() :: String.t()
+  @type user_schema() :: atom()
+
+  @type email() :: String.t()
+
+  @type password() :: String.t()
+
+  @type password_hash() :: String.t()
+
+  @type user() :: %{
+          email: email(),
+          password: password(),
+          password_hash: password_hash(),
+          email_verification_token: Stein.uuid(),
+          email_verified_at: DateTime.t()
+        }
 
   @doc """
   Hash the changed password in a changeset
 
   - Skips if the changeset is invalid
   - Skips if a password is not changed
+  - Hashes the password with BCrypt otherwise
+
+  Requires the user schema to contain:
+  - `password`, type `:string`
+  - `password_hash`, type `:string`
   """
+  @spec hash_password(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   def hash_password(changeset) do
     case changeset.valid? do
       true ->
@@ -34,8 +64,14 @@ defmodule Stein.Accounts do
   end
 
   @doc """
-  Validate a login
+  Validate a email and password match a user
+
+  Requires the user schema to contain:
+  - `email`, type `:string`
+  - `password_hash`, type `:string`
   """
+  @spec validate_login(Stein.repo(), user_schema(), email(), password()) ::
+          {:error, :invalid} | {:ok, user()}
   def validate_login(repo, struct, email, password) do
     case repo.get_by(struct, email: email) do
       nil ->
@@ -61,8 +97,13 @@ defmodule Stein.Accounts do
   Verify a user's email address from a token sent to their email address
 
   This token should be a UUID, if it is not `{:error, :invalid}` will be returned.
+
+  Requires the user schema to contain:
+  - `email_verification_token`, type `:uuid`
+  - `email_verified_at`, type `:utc_datetime`
   """
-  @spec verify_email(repo(), schema(), uuid()) :: {:ok, schema()} | {:error, changeset()}
+  @spec verify_email(Stein.repo(), user_schema(), Stein.uuid()) ::
+          {:ok, user()} | {:error, :invalid} | {:error, Ecto.Changeset.t()}
   def verify_email(repo, struct, token) do
     case Ecto.UUID.cast(token) do
       {:ok, token} ->
@@ -96,6 +137,9 @@ defmodule Stein.Accounts do
       iex> Accounts.email_verified?(user)
       false
   """
+  @spec email_verified?(user()) :: boolean()
+  def email_verified?(user)
+
   def email_verified?(%{email_verified_at: verified_at}) when verified_at != nil, do: true
 
   def email_verified?(_), do: false
