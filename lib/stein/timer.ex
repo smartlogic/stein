2b88bff434b846b41749ab9a14f07ae3e3f7a1c4
Eric Oestrich <eric@oestrich.org>
2019-10-29 14:34:01 -0400

    Calculate the delay to the next weekly run

--- lib/stein/timer.ex
+++ lib/stein/timer.ex
@@ -15,7 +15,7 @@ defmodule Stein.Timer do
   @spec calculate_daily_cycle_delay(DateTime.t(), runs_at_opts()) :: milliseconds()
   def calculate_daily_cycle_delay(now, runs_at) do
     hour = Keyword.get(runs_at, :hour, 0)
-    minute = Keyword.get(runs_at, :mintue, 0)
+    minute = Keyword.get(runs_at, :minute, 0)
     second = Keyword.get(runs_at, :second, 0)
 
     now
@@ -31,6 +31,46 @@ defmodule Stein.Timer do
 
       false ->
         Timex.shift(next_run, days: 1)
+    end
+  end
+
+  @doc """
+  Calculate the delay to the next weekly cycle
+  """
+  @spec calculate_weekly_cycle_delay(DateTime.t(), runs_at_opts()) :: milliseconds()
+  def calculate_weekly_cycle_delay(now, runs_at) do
+    day = Keyword.get(runs_at, :day, 0)
+    hour = Keyword.get(runs_at, :hour, 0)
+    minute = Keyword.get(runs_at, :minute, 0)
+    second = Keyword.get(runs_at, :second, 0)
+
+    now
+    |> Timex.set(hour: hour, minute: minute, second: second)
+    |> shift_days(day)
+    |> maybe_shift_a_week(now)
+    |> Timex.diff(now, :milliseconds)
+  end
+
+  defp shift_days(now, day) do
+    case Timex.weekday(now) > day do
+      true ->
+        # Shift into the next week
+        # time to end of the week plus the day
+        Timex.shift(now, days: day + (7 - Timex.weekday(now)))
+
+      false ->
+        # Shift ahead in this week
+        Timex.shift(now, days: day - Timex.weekday(now))
+    end
+  end
+
+  defp maybe_shift_a_week(next_run, now) do
+    case Time.before?(now, next_run) do
+      true ->
+        next_run
+
+      false ->
+        Timex.shift(next_run, days: 7)
     end
   end
 end
--- test/stein/timer_test.exs
+++ test/stein/timer_test.exs
@@ -3,7 +3,7 @@ defmodule Stein.TimerTest do
 
   alias Stein.Timer
 
-  describe "calculating the delay to the next run" do
+  describe "calculating the delay to the next daily run" do
     test "for the next cycle" do
       now =
         Timex.now()
@@ -24,6 +24,64 @@ defmodule Stein.TimerTest do
       delay = Timer.calculate_daily_cycle_delay(now, hour: 6)
 
       assert delay == 3600 * 2 * 1000
+    end
+  end
+
+  describe "calculating the delay to the next weekly run" do
+    test "for the next cycle" do
+      # now is thursday, shift to monday
+
+      now =
+        Timex.now()
+        |> Timex.set(year: 2019, month: 10, day: 24, hour: 10, minute: 0, second: 0)
+        |> DateTime.truncate(:second)
+
+      delay = Timer.calculate_weekly_cycle_delay(now, day: 1, hour: 11)
+
+      # four days and an hour in milliseconds
+      assert delay == 4 * 24 * 3600 * 1000 + 3600 * 1000
+    end
+
+    test "for the current cycle same day" do
+      # now is thursday, shift to saturday
+
+      now =
+        Timex.now()
+        |> Timex.set(year: 2019, month: 10, day: 24, hour: 10, minute: 0, second: 0)
+        |> DateTime.truncate(:second)
+
+      delay = Timer.calculate_weekly_cycle_delay(now, day: 4, hour: 11)
+
+      # an hour in milliseconds
+      assert delay == 3600 * 1000
+    end
+
+    test "for the current cycle same day, passed the timer" do
+      # now is thursday, shift to saturday
+
+      now =
+        Timex.now()
+        |> Timex.set(year: 2019, month: 10, day: 24, hour: 12, minute: 0, second: 0)
+        |> DateTime.truncate(:second)
+
+      delay = Timer.calculate_weekly_cycle_delay(now, day: 4, hour: 11)
+
+      # six days, 23 hours in milliseconds
+      assert delay == 6 * 24 * 3600 * 1000 + 23 * 3600 * 1000
+    end
+
+    test "for the current cycle" do
+      # now is thursday, shift to saturday
+
+      now =
+        Timex.now()
+        |> Timex.set(year: 2019, month: 10, day: 24, hour: 10, minute: 0, second: 0)
+        |> DateTime.truncate(:second)
+
+      delay = Timer.calculate_weekly_cycle_delay(now, day: 6, hour: 11)
+
+      # two days and an hour in milliseconds
+      assert delay == 2 * 24 * 3600 * 1000 + 3600 * 1000
     end
   end
 end
