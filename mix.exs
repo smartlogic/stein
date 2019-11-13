267cb5ae4535aeff8821517f5a172ee12f8f0d7b
Eric Oestrich <eric@oestrich.org>
2019-11-13 18:35:49 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stein.MixProject do
   def project do
     [
       app: :stein,
-      version: "0.4.1",
+      version: "0.5.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
